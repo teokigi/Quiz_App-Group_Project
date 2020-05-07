3b7ffe8de8d40f7929d009a55f2c1fa5d858ac01
Christopher Pearson <cpearson3140@gmail.com>
2020-05-07 13:40:28 +0100

    Populated seeds.js, create_router.js and server.js files

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('.helper/create_router.js');
+
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('topics');
+    const questionsColelction = db.collection('questions');
+    const questionsRouter = createRouter(questionsCollection);
+    app.use('/api/questions', questionsRouter);
+  })
+  .catch(console.err);
+
+  app.listen(3000, function () {
+    console.log(`Listening on port ${ this.address().port }`);
+  });
