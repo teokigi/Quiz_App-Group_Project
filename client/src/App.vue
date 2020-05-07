124ac31468475d9ce82fbe74a744687b026c77b7
Michael Wales <michaelwales@gmail.com>
2020-05-07 15:08:31 +0100

    Adds Navigation and Contextual Body

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,47 @@
 <template lang="html">
   <div>
-    <page-header />
+    <section>
+      <page-header />
+    </section>
+    <div class="body">
+      <revision v-if="viewSelector === 1" />
+      <quiz v-if="viewSelector === 2" />
+      <stats v-if="viewSelector === 3" />
+    </div>
   </div>
 </template>
 
 <script>
 import PageHeader from '@/components/PageHeader.vue';
-
+import {eventBus} from '@/main.js';
+import Quiz from '@/components/Quiz.vue';
+import Revision from '@/components/Revision.vue';
+import Stats from '@/components/Stats.vue';
 
 export default {
   name: 'app',
   components: {
-    'page-header': PageHeader
+    'page-header': PageHeader,
+    'quiz': Quiz,
+    'revision': Revision,
+    'stats': Stats
+  },
+  data() {
+    return {
+      viewSelector: 0
+    }
+  },
+  mounted() {
+    eventBus.$on('selected-nav-revision', (navNumber) => {
+      this.viewSelector = navNumber
+    })
+    eventBus.$on('selected-nav-quiz', (navNumber) => {
+      this.viewSelector = navNumber
+    })
+    eventBus.$on('selected-nav-stats', (navNumber) => {
+      this.viewSelector = navNumber
+    })
   }
-
 }
 </script>
 
