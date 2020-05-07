349ba59f00788bcbe0f3bc92681cf174651502c5
Michael Wales <michaelwales@gmail.com>
2020-05-07 15:57:55 +0100

    Adds setup for Revision component

--- client/src/components/Revision.vue
+++ client/src/components/Revision.vue
@@ -1,6 +1,11 @@
 <template lang="html">
   <div class="">
-    Revise
+    <h2>Revise</h2>
+    <label for="revision">Select a Topic:</label>
+    <select id="revision" v-on:change="topicSelected" v-model="selectedTopic">
+      <option disabled value="">select a topic...</option>
+      <option v-for="category in "></option>
+    </select>
   </div>
 </template>
 
