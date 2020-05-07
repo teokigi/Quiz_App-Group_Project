8f39c8192b59fc4a00e1f1016b424e5b407a6a67
Michael Wales <michaelwales@gmail.com>
2020-05-07 13:39:44 +0100

    Adds PageHeader.vue and imports to App.vue

---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="header">
+    <h1>Quizia</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-header'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
