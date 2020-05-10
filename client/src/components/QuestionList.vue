ea26e238d5cb591d4c6e4165df692913dbf60945
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-10 21:55:37 +0100

    questions styling

--- client/src/components/QuestionList.vue
+++ client/src/components/QuestionList.vue
@@ -1,14 +1,14 @@
 <template lang="html">
-  <div>
+  <div class="QL">
     <form v-on:submit.prevent="handleSubmit()" class="formWrapper" v-if="!completedTest">
-        <div class="question" v-for="(question, qindex) of this.topic.questions">
-            Q{{qindex+1}}. {{question.question}}<br>
-            <label v-for="(answer, aindex) of question.answers" class="answerLabel">
-                <input type="radio" :name="'drone'+ qindex" v-model="answers[qindex]" v-bind:value="answer" required>
-                {{answer}}<br>
-            </label>
-        </div>
-      <button type="submit">Submit Answers</button>
+            <div class="question" v-for="(question, qindex) of this.topic.questions">
+                Q{{qindex+1}}. {{question.question}}<br>
+                <label v-for="(answer, aindex) of question.answers" class="answerLabel">
+                    <input type="radio" :name="'drone'+ qindex" v-model="answers[qindex]" v-bind:value="answer" required>
+                    {{answer}}<br>
+                </label>
+            </div>
+          <button type="submit">Submit Answers</button>
     </form>
     <div v-if="completedTest" class="formWrapper">
       Here are your results
@@ -64,18 +64,26 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.QL{
+    height:75vh;
+    overflow-y:scroll;
+}
 .formWrapper{
     display:flex;
     flex-direction:column;
     align-items:center;
+
 }
+
 .question{
+    box-sizing:border-box;
     margin:10px;
-    padding:10px;
+    padding:5px;
+    padding-left:10px;
     text-align:left;
     width:40%;
     border-style:solid;
-    border-radius:30px;
+    border-radius:20px;
     background-color:lightblue;
     opacity:0.75;
 }
@@ -89,8 +97,18 @@ button {
     background-color:lightblue;
     box-shadow:8px 8px 2px black;
 }
+input{
+    cursor:inherit;
+}
+button:hover{
+    cursor:inherit;
+}
+.answerLabel{
+    font-size:15px;
+}
 .answerLabel:hover{
     color:blue;
+    cursor:inherit;
 }
 .correct{
     background-color:lightgreen;
