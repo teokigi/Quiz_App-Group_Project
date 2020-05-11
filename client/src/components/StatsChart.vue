414402e9301993e4aa296db69fd3cd9f9d05b48b
Michael Wales <michaelwales@gmail.com>
2020-05-11 12:31:22 +0100

    Beautifies Chart Code

--- client/src/components/StatsChart.vue
+++ client/src/components/StatsChart.vue
@@ -10,47 +10,48 @@
 import Stats from '@/components/Stats.vue';
 
 export default {
-    name: 'stats-chart',
-    props: ['user'],
-    data() {
+  name: 'stats-chart',
+  props: ['user'],
+  data() {
     return {
-        chartOptions: {
-            chart: {
-              type: 'column'
-            },
-            title: {
-              text: this.user.nickname + "'s Stats"
-            },
-            yAxis:{
-                title:{
-                    text:'Score'}
-            },
-            xAxis:{
-                categories:[this.user.answerSet[0].category,this.user.answerSet[1].category,this.user.answerSet[2].category]
-            },
+      chartOptions: {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: this.user.nickname + "'s Stats"
+        },
+        yAxis: {
+          title: {
+            text:'Score'
+          }
+        },
+        xAxis: {
+          categories:[this.user.answerSet[0].category,this.user.answerSet[1].category,this.user.answerSet[2].category]
+        },
             series: [{
-                name:'Correct',
-                data:[
+              name:'Correct',
+              data:[
                     {name:this.user.answerSet[0].category,y:this.user.answerSet[0].correctAnswers},
                     {name:this.user.answerSet[1].category,y:this.user.answerSet[1].correctAnswers},
                     {name:this.user.answerSet[2].category,y:this.user.answerSet[2].correctAnswers}
-                ],
-                color:'#5ca230'
+              ],
+              color:'#5ca230'
             },
             {
-                name:'Incorrect',
-                data:[
-                    {name:this.user.answerSet[0].category,y:this.user.answerSet[0].incorrectAnswers},
-                    {name:this.user.answerSet[1].category,y:this.user.answerSet[1].incorrectAnswers},
-                    {name:this.user.answerSet[2].category,y:this.user.answerSet[2].incorrectAnswers},
-                ],
-                color:'#f18a1a'
+              name:'Incorrect',
+              data:[
+                {name:this.user.answerSet[0].category,y:this.user.answerSet[0].incorrectAnswers},
+                {name:this.user.answerSet[1].category,y:this.user.answerSet[1].incorrectAnswers},
+                {name:this.user.answerSet[2].category,y:this.user.answerSet[2].incorrectAnswers},
+              ],
+              color:'#f18a1a'
             }
-        ]
-        }
+          ]
+      }
     }
-    }
-    }
+  }
+}
 
 </script>
 
