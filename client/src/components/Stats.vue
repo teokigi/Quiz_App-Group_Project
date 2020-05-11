665a9347f0b6beb487420eb5bd49d580822253dd
Michael Wales <michaelwales@gmail.com>
2020-05-11 11:42:22 +0100

    Adds Chart but needs fixin'

--- client/src/components/Stats.vue
+++ client/src/components/Stats.vue
@@ -1,17 +1,31 @@
 <template lang="html">
   <div class="statsWrapper">
-    
+
+    <div v-if="!selectedUser">
+      <label for="revision">Select a User: </label>
+      <select id="revision" v-model="selectedUser">
+        <option disabled value="">select a user</option>
+        <option v-for="user of users" :value="user">{{user.nickname}}</option>
+      </select>
+    </div>
+    <stats-chart v-if="selectedUser" :user="selectedUser" />
   </div>
 </template>
 
 
 <script>
-import {eventBus} from '@/main.js';
-import {Chart} from 'highcharts-vue';
+import StatsChart from '@/components/StatsChart.vue';
 
 export default {
+  name: 'stats',
+  props: ['users'],
   components: {
-    'highcharts': Chart
+    'stats-chart': StatsChart
+  },
+  data() {
+    return {
+      selectedUser: null
+    }
   }
 }
 </script>
--- client/src/components/StatsChart.vue
+++ client/src/components/StatsChart.vue
@@ -1,14 +1,34 @@
 <template lang="html">
   <div class="selectedStatsWrapper">
 
+    <highcharts :options="chartOptions"></highcharts>
+
+  </div>
 </template>
 
 <script>
+import Stats from '@/components/Stats.vue';
 
 export default {
   name: 'stats-chart',
-  props: ['stats']
-
+  props: ['user'],
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: this.user.nickname + "'s Stats"
+        },
+        columns: [
+            [null, this.user.answerSet[0].category, this.user.answerSet[1].category, this.user.answerSet[2].category],
+            ['Correct', this.user.answerSet[0].correctAnswers, this.user.answerSet[1].correctAnswers, this.user.answerSet[2].correctAnswers],
+            ['Incorrect', this.user.answerSet[0].incorrectAnswers, this.user.answerSet[1].incorrectAnswers, this.user.answerSet[2].incorrectAnswers]
+        ]
+      }
+    }
+  }
 }
 </script>
 
