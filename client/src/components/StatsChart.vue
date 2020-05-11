2526a9823562efeee4df28e5ae3bbefe9ed912d8
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-11 12:24:11 +0100

    highcharts working with a little css

--- client/src/components/StatsChart.vue
+++ client/src/components/StatsChart.vue
@@ -10,27 +10,52 @@
 import Stats from '@/components/Stats.vue';
 
 export default {
-  name: 'stats-chart',
-  props: ['user'],
-  data() {
+    name: 'stats-chart',
+    props: ['user'],
+    data() {
     return {
-      chartOptions: {
-        chart: {
-          type: 'column'
-        },
-        title: {
-          text: this.user.nickname + "'s Stats"
-        },
-        columns: [
-            [null, this.user.answerSet[0].category, this.user.answerSet[1].category, this.user.answerSet[2].category],
-            ['Correct', this.user.answerSet[0].correctAnswers, this.user.answerSet[1].correctAnswers, this.user.answerSet[2].correctAnswers],
-            ['Incorrect', this.user.answerSet[0].incorrectAnswers, this.user.answerSet[1].incorrectAnswers, this.user.answerSet[2].incorrectAnswers]
+        chartOptions: {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: this.user.nickname + "'s Stats"
+            },
+            yAxis:{
+                title:{
+                    text:'Score'}
+            },
+            xAxis:{
+                categories:[this.user.answerSet[0].category,this.user.answerSet[1].category,this.user.answerSet[2].category]
+            },
+            series: [{
+                name:'Correct',
+                data:[
+                    {name:this.user.answerSet[0].category,y:this.user.answerSet[0].correctAnswers},
+                    {name:this.user.answerSet[1].category,y:this.user.answerSet[1].correctAnswers},
+                    {name:this.user.answerSet[2].category,y:this.user.answerSet[2].correctAnswers}
+                ],
+                color:'#5ca230'
+            },
+            {
+                name:'Incorrect',
+                data:[
+                    {name:this.user.answerSet[0].category,y:this.user.answerSet[0].incorrectAnswers},
+                    {name:this.user.answerSet[1].category,y:this.user.answerSet[1].incorrectAnswers},
+                    {name:this.user.answerSet[2].category,y:this.user.answerSet[2].incorrectAnswers},
+                ],
+                color:'#f18a1a'
+            }
         ]
-      }
+        }
     }
-  }
-}
+    }
+    }
+
 </script>
 
 <style lang="css" scoped>
+.selectedStatsWrapper{
+    margin:30px;
+}
 </style>
