0caf09f215e388829b254518ef482eb0d02be61d
Michael Wales <michaelwales@gmail.com>
2020-05-11 12:32:47 +0100

    Beautifies Chart Code

--- client/src/components/StatsChart.vue
+++ client/src/components/StatsChart.vue
@@ -29,25 +29,26 @@ export default {
         xAxis: {
           categories:[this.user.answerSet[0].category,this.user.answerSet[1].category,this.user.answerSet[2].category]
         },
-            series: [{
-              name:'Correct',
-              data:[
-                    {name:this.user.answerSet[0].category,y:this.user.answerSet[0].correctAnswers},
-                    {name:this.user.answerSet[1].category,y:this.user.answerSet[1].correctAnswers},
-                    {name:this.user.answerSet[2].category,y:this.user.answerSet[2].correctAnswers}
-              ],
-              color:'#5ca230'
-            },
-            {
-              name:'Incorrect',
-              data:[
-                {name:this.user.answerSet[0].category,y:this.user.answerSet[0].incorrectAnswers},
-                {name:this.user.answerSet[1].category,y:this.user.answerSet[1].incorrectAnswers},
-                {name:this.user.answerSet[2].category,y:this.user.answerSet[2].incorrectAnswers},
-              ],
-              color:'#f18a1a'
-            }
-          ]
+        series: [
+          {
+            name:'Correct',
+            data:[
+              {name:this.user.answerSet[0].category,y:this.user.answerSet[0].correctAnswers},
+              {name:this.user.answerSet[1].category,y:this.user.answerSet[1].correctAnswers},
+              {name:this.user.answerSet[2].category,y:this.user.answerSet[2].correctAnswers}
+            ],
+            color:'#5ca230'
+          },
+          {
+            name:'Incorrect',
+            data:[
+              {name:this.user.answerSet[0].category,y:this.user.answerSet[0].incorrectAnswers},
+              {name:this.user.answerSet[1].category,y:this.user.answerSet[1].incorrectAnswers},
+              {name:this.user.answerSet[2].category,y:this.user.answerSet[2].incorrectAnswers},
+            ],
+            color:'#f18a1a'
+          }
+        ]
       }
     }
   }
