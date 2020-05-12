eced23db375ee12f317fe0a3649304ec62a4937c
Michael Wales <michaelwales@gmail.com>
2020-05-12 14:57:25 +0100

    Adds full functionality for user persistence and user logging in each view

--- client/src/components/Stats.vue
+++ client/src/components/Stats.vue
@@ -1,14 +1,6 @@
 <template lang="html">
   <div class="statsWrapper">
-
-    <div v-if="!selectedUser">
-      <label for="revision">Select a User: </label>
-      <select id="revision" v-model="selectedUser">
-        <option disabled value="">select a user</option>
-        <option v-for="user of users" :value="user">{{user.nickname}}</option>
-      </select>
-    </div>
-    <stats-chart v-if="selectedUser" :user="selectedUser" />
+    <stats-chart v-if="currentUser" :currentUser="currentUser" />
   </div>
 </template>
 
@@ -21,11 +13,6 @@ export default {
   props: ['users', 'currentUser'],
   components: {
     'stats-chart': StatsChart
-  },
-  data() {
-    return {
-      selectedUser: null
-    }
   }
 }
 </script>
